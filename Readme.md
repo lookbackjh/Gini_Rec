446913b958acd64db892e68742ccd10e8f981922
lookbackjh <jjjjunho1011@gmail.com>
2023-09-21 09:15:44 +0000

    updatereadme

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to Use
+
+## 1. Data Generator
+
+As there are too many data in the dataset we only select some of the top 1000 user 
+and use their data to train. 
+to see the details of how we have made the `order_info_frequency_1000.pickle` data, check `datagenerator.ipynb`
+
+## 2. Model Train and Recommendation
+`recommender.py`  recommends the product based on the model you've trained by KNN based algorithm that we have made
+to
+returning scores and further logics will be updated 
--- recommender.py
+++ recommender.py
@@ -1,1 +1,56 @@
+import argparse
+from src.datagenerator import DataGenerator
+from src.model import KNNRecommender
+import pandas as pd
+import tqdm
+import numpy as np
 
+def parser():
+
+    args = argparse.ArgumentParser()
+    args.add_argument('--train_ratio', type=float, default=0.8)
+    args.add_argument('--seed', type=int, default=42)
+    args.add_argument('--k', type=int, default=20)
+    args.add_argument('--topk', type=int, default=5)
+    args.add_argument('--cur_year', type=int, default=2023)
+    args.add_argument('--user_id', type=int, default=7)
+    args = args.parse_args()
+    return args
+
+def recommender(args,df):
+    datagenerator = DataGenerator(args,df)
+    matrix_train,matrix_test=datagenerator.generate_user_item_matrix()
+    gendermap,agemap=datagenerator.user_feature_dictionary()
+
+    precisions=[]
+    recalls=[]
+
+    for userid in tqdm.tqdm(list(gendermap.keys())[:]):
+        args.user_id=userid 
+        rec = KNNRecommender(args,matrix_train,matrix_test,agemap,gendermap)
+        product,score,precsion,recall=rec.recommend() # user_id = 7
+        precisions.append(precsion)
+        recalls.append(recall)
+        #f1scores.append(f1score)
+
+    print("precision: ",np.mean(np.array(precisions)))
+    print("recall: ",np.mean(np.array(recalls)))
+    #print("f1score: ",np.mean(np.array(f1scores)))
+
+
+    # args.user_id=7 #predefined user_id
+    # rec = KNNRecommender(args,matrix_train,matrix_test,agemap,gendermap)
+    # product,score,precsion,recall, f1score=rec.recommend() # user_id = 7
+    # print("recommended products: ",product)
+    # print("recommended score: ",score)
+    # print("precision: ",precsion)
+    # print("recall: ",recall)
+    # print("f1score: ",f1score)
+    pass
+
+
+
+if __name__ == "__main__":
+    args = parser()
+    df = pd.read_pickle("order_info_frequency_1000.pickle")
+    recommender(args,df)
